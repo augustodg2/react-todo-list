8f8b88f8b8980cc9ce4ca67c017884db77e56387
Augusto Dias <augustodg2@gmail.com>
2020-02-17 16:56:26 -0300

    + Extract TodoItem css

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,88 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './TodoItem.css';
 
 export default class TodoItem extends Component {
-    state = {
-        showDeleteButton: false,
-        hoverDeleteButton: false,
-    }
-    
-    getStyle = () => {
-        return {
-            textDecoration: this.props.todo.completed ? 'line-through' : 'none',
-            color: this.props.todo.completed ? '#ccc' : 'black',
-            cursor: 'pointer',
-            padding: '1rem',
-            paddingLeft: '1.5rem',
-            display: 'flex',
-            flex: 4
-        }
-    }
-    
     render() {
         const { id, title, completed } = this.props.todo;
 
         return (
-            <div style={ this.containerStyle() }
-                 onMouseEnter={ this.toggleDeleteButton.bind(this, id) }
-                 onMouseLeave={ this.toggleDeleteButton.bind(this, id) }
-                 >
-                <div style={ this.getStyle() }
-                    onClick={ this.props.toggleComplete.bind(this, id) }
-                    >
-                    <p>
-                        <input type="checkbox"
-                            style={{ marginRight: '.75rem' }}
-                            readOnly={ true }
-                            checked={ completed }
-                            />
+            <div
+                className={ `todoItem ${completed ? 'completed' : '' }` }
+            >
+                <div>
+                    <input
+                        id={ `${id}completed` }
+                        type="checkbox"
+                        checked={ completed }
+                        onChange={ this.props.toggleComplete.bind(this, id) }
+                    />
+                    <label htmlFor={ `${id}completed` }>
                         { title }
-                    </p>
+                    </label>    
                 </div>
-                <div style={ this.buttonStyle() }
-                    onMouseEnter={ this.toggleHoverDelete }
-                    onMouseLeave={ this.toggleHoverDelete }
+                <div
+                    className="deleteButton"
                     onClick={ this.props.deleteTodo.bind(this, id) }
                 >
                     <i className="material-icons">delete_outline</i>
                 </div>
             </div>
+
         )
-    }
-
-    
-    toggleDeleteButton = () => {
-        this.setState({ 
-            showDeleteButton: !this.state.showDeleteButton
-            // showDeleteButton: true
-        });
-    }
-
-    toggleHoverDelete = () => {
-        this.setState({ hoverDeleteButton: !this.state.hoverDeleteButton });
-    }
-    
-    buttonStyle = () => {
-        return {
-            background: this.state.hoverDeleteButton ? '#fafafa' : 'none',
-            border: 'none',
-            color: this.state.hoverDeleteButton ? '#999' : '#ccc',
-            display: this.state.showDeleteButton ? 'flex' : 'none',
-            cursor: 'pointer',
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
-        }
-    } 
-
-    containerStyle = () => {
-        return {
-            display: 'flex',
-            width: '100vw',
-            borderBottom: '1px #eee solid',
-            background: this.props.todo.completed ? '#fafafa' : '#fff',
-            alignItems: 'stretch',
-        }
     }
 }
 
