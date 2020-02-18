763c80bd74210ecba69c3fe79e2f45af09e795dc
Augusto Dias <augustodg2@gmail.com>
2020-02-18 11:37:58 -0300

    + Extract Header.js style

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,33 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import AddTodo from '../AddTodo'
+import './Header.css'
 
 class Header extends React.Component {
   render () {
     return (
-      <header style={ headerStyle }>
+      <header className="header">
         <h2>Todo List</h2>
-        <small style={ smallStyle }>Developed by Augusto Dias</small>
+        <small className="small">Developed by Augusto Dias</small>
 
         <AddTodo addTodo={ this.props.addTodo } />
       </header>
     )
   }
-}
-
-const headerStyle = {
-  padding: '2rem 1.5rem 1.5rem 1.5rem',
-  boxShadow: '0px 1px 5px #ccc',
-  position: 'relative',
-  zIndex: 2,
-  color: '#444'
-}
-
-const smallStyle = {
-  fontFamily: 'Helvetica Neue',
-  color: '#aaa',
-  display: 'inline-block',
-  paddingBottom: '1.25rem'
 }
 
 Header.propTypes = {
