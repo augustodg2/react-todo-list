a8ed51ae5991ceb3ec6d1f849f8687fe026ae2f7
Augusto Dias <augustodg2@gmail.com>
2020-03-05 15:28:33 -0300

    Modified header style

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 import './Header.css'
 
 class Header extends React.Component {
   render () {
     return (
       <header className="header">
-        <h2><Link to="/">Todo List</Link></h2>
-        <nav className="nav-links">
-          <Link to="/about">About</Link>
-        </nav>
+        <h2>Primary</h2>
       </header>
     )
   }
