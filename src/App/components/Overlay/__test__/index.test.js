85e13d20362435ee47099bd3a4ee99ff8af0c2e8
Augusto Dias <augustodg2@gmail.com>
2020-04-08 10:51:17 -0300

    Initial overlay test

---
+++ src/App/components/Overlay/__test__/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import Button from '../index'
+
+afterEach(cleanup)
+
+describe('# Overlay', () => {
+  it('should render children', () => {
+
+  })
+})
