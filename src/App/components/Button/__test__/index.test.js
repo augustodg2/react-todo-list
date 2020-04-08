40d5874383992f33f334f62b8c15914e23de1c85
Augusto Dias <augustodg2@gmail.com>
2020-04-08 10:14:18 -0300

    Fix unused vars

--- src/App/components/Button/__test__/index.test.js
+++ src/App/components/Button/__test__/index.test.js
@@ -6,38 +6,31 @@ afterEach(cleanup)
 
 describe('# Button', () => {
   it('should render text from props', () => {
-    const { getByTestId } = render(
-      <Button text="click me" />
-    )
+    const { getByTestId } = render(<Button text="click me" />)
     const buttonText = getByTestId('button__text')
+
     expect(buttonText.textContent).toBe('click me')
   })
 
-  it.only('should render icon from props', () => {
+  it('should render icon from props', () => {
     const Icon = jest.fn(() => <p>icon</p>)
-    const { getByTestId } = render(
-      <Button text="click me" icon={<Icon />} />
-    )
+    render(<Button text="click me" icon={<Icon />} />)
+
     expect(Icon).toHaveBeenCalled()
   })
 
-  // Button variants
   it('should add variant classnames to button', () => {
-    const { getByTestId } = render(
-      <Button variant="abc abcd" />
-    )
+    const { getByTestId } = render(<Button variant="abc abcd" />)
     const button = getByTestId('button')
+
     expect(button.className).toContain('abc abcd')
   })
 
-  // Action
   it('should execute action on click', () => {
     const action = jest.fn()
-    const { getByTestId } = render(
-      <Button action={action} />
-    )
-
+    const { getByTestId } = render(<Button action={action} />)
     const button = getByTestId('button')
+
     fireEvent.click(button)
 
     expect(action).toBeCalledTimes(1)
